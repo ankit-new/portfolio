0cd35796dab2003a89f212f76d2b7e6ddeca7fb1
Ankit <messiank10@gmail.com>
2024-10-21 18:36:26 +0530

    added gym app

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,6 +4,7 @@ import Portfolio from "../assets/Portfolio.png";
 import Cocktaildb from "../assets/Cocktaildb.png";
 import store from "../assets/store.png";
 import Udemy from "../assets/Udemy.png";
+import gym from "../assets/gym.png"
 
 const Work = () => {
   return (
@@ -17,7 +18,7 @@ const Work = () => {
         </div>
 
         {/* Container */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2">
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${Netflix})` }}
@@ -121,6 +122,35 @@ const Work = () => {
                 </a>
                 <a
                   href="https://github.com/ankit-new/shop-react" target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${gym})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+              Gym App
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://stately-faloodeh-e7cda4.netlify.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/ankit-new/Gym-app" target="_blank"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
